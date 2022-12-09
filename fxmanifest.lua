84b8443ceb7bf5b9c43adbef0d17343c22511a67
JJ Case <jjcasegaming@yahoo.com>
2022-12-08 19:47:30 -0800

    edited to retributionRP

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,3 +18,12 @@ files {
     "html/**/*"
 }
 dependency '/assetpacks'
+
+-- Tell server we will close the loading screen resource ourselves
+loadscreen_manual_shutdown "yes"
+-- Client Script
+client_script "client.lua"
+
+fx_version "bodacious"
+
+game "gta5"
